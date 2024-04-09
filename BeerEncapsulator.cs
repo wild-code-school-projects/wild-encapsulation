40e15282515cfcde99f212e1529b41aac2d08502
SuperCoolNinja <sbpro570@gmail.com>
2024-04-09 14:51:05 +0200

    init

---
+++ BeerEncapsulator.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Encapsulation;
+
+internal class BeerEncapsulator
+{
+
+    public int AvailableCapsules
+    {
+        get => _avalaibleCapsules;
+        set
+        {
+            if (value > 0)
+                _avalaibleCapsules = value;
+        }
+    }
+
+    public int AvailableBottles
+    {
+        get => _avalaibleBottles;
+        set
+        {
+            if (value > 0)
+                _avalaibleBottles = value;
+        }
+    }
+
+    public double AvailableBeerVolume
+    {
+        get => _avalaibleBeerVolume;
+        private set
+        {
+            if (value > 0)
+                _avalaibleBeerVolume = value;
+        }
+    }
+
+    public void AddBeer(double volume)
+    {
+        AvailableBeerVolume += volume;
+    }
+
+    public int ProduceEncapsulatedBeerBottles(int numBottles)
+    {
+        int bottlesProduced = 0;
+
+        double requiredBeerVolume = numBottles * 33;
+
+        if (AvailableBottles >= numBottles && AvailableCapsules >= numBottles && AvailableBeerVolume >= requiredBeerVolume)
+        {
+            bottlesProduced = numBottles;
+            AvailableBeerVolume -= requiredBeerVolume;
+            AvailableBottles -= numBottles;
+            AvailableCapsules -= numBottles;
+            Console.WriteLine($"{numBottles} bottles of beer produced and encapsulated.");
+        }
+        else Console.WriteLine("Insufficient components to produce the requested number of bottles.");
+
+        return bottlesProduced;
+    }
+
+    // représente en centilitres le volume de bière disponible dans la machine :
+    private double _avalaibleBeerVolume;
+
+    // représente le nombre de bouteilles disponibles :
+    private int _avalaibleBottles;
+
+    // représente le nombre de capsules disponibles :
+    private int _avalaibleCapsules;
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BeerEncapsulator beerEncapsulator = new BeerEncapsulator();
+            beerEncapsulator.AvailableBottles = 5;
+            beerEncapsulator.AvailableCapsules = 5;
+            
+            beerEncapsulator.AddBeer(33f);
+
+
+            int qtyBottle = beerEncapsulator.ProduceEncapsulatedBeerBottles(1);
+
+            Console.WriteLine("Capsule Quantity available : " + beerEncapsulator.AvailableCapsules);
+            Console.WriteLine("Beer Quantity available : " + beerEncapsulator.AvailableBeerVolume);
+            Console.WriteLine("Bottle Quantity available : " + qtyBottle);
+
+
+
+        }
+    }
+}
